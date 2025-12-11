c0d8e1d96457a60484742574543bf3f66b051d3f
S Punkt Online <109141465+spo-digital@users.noreply.github.com>
2025-12-11 10:02:20 +0100

    Add files via upload

---
+++ SpoCustomerOrderCountSync/src/SpoCustomerOrderCountSync.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace SpoCustomerOrderCountSync;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Context\UpdateContext;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+
+class SpoCustomerOrderCountSync extends Plugin
+{
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
+        $loader->load('services.xml');
+    }
+
+    public function install(InstallContext $installContext): void
+    {
+        // Do stuff such as creating a new payment method
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        parent::uninstall($uninstallContext);
+
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+
+        // Remove or deactivate the data created by the plugin
+    }
+
+    public function activate(ActivateContext $activateContext): void
+    {
+        // Activate entities, such as a new payment method
+        // Or create new entities here, because now your plugin is installed and active for sure
+    }
+
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        // Deactivate entities, such as a new payment method
+        // Or remove previously created entities
+    }
+
+    public function update(UpdateContext $updateContext): void
+    {
+        // Update necessary stuff, mostly non-database related
+    }
+
+    public function postInstall(InstallContext $installContext): void
+    {
+    }
+
+    public function postUpdate(UpdateContext $updateContext): void
+    {
+    }
+}
+
+
+
+
---
+++ SpoCustomerOrderCountSync/src/Subscriber/CustomerOrderCountSyncSubscriber.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace SpoCustomerOrderCountSync\Subscriber;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\System\SalesChannel\Event\SalesChannelContextSwitchEvent;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\System\Customer\CustomerEntity;
+use Psr\Log\LoggerInterface;
+
+class CustomerOrderCountSyncSubscriber implements EventSubscriberInterface
+{
+    private EntityRepository $customerRepository;
+    private LoggerInterface $logger;
+
+    public function __construct(EntityRepository $customerRepository, LoggerInterface $logger)
+    {
+        $this->customerRepository = $customerRepository;
+        $this->logger = $logger;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SalesChannelContextSwitchEvent::class => 'onSalesChannelContextSwitch',
+        ];
+    }
+
+    public function onSalesChannelContextSwitch(SalesChannelContextSwitchEvent $event): void
+    {
+        $context = $event->getSalesChannelContext()->getContext();
+        $customer = $event->getSalesChannelContext()->getCustomer();
+
+        if (!$customer instanceof CustomerEntity) {
+            return;
+        }
+
+        // Lade Customer mit order_count
+        $criteria = new Criteria([$customer->getId()]);
+        $criteria->addFilter(new EqualsFilter('id', $customer->getId()));
+        $criteria->addFields(['id', 'orderCount']);
+
+        $result = $this->customerRepository->search($criteria, $context)->first();
+
+        if (!$result) {
+            $this->logger->warning('Customer not found in repository.', [
+                'customerId' => $customer->getId()
+            ]);
+            return;
+        }
+
+        $orderCount = $result->getOrderCount();
+
+        // 📝 Logge den geladenen Wert
+        $this->logger->info('Order count loaded', [
+            'customerId' => $customer->getId(),
+            'orderCount' => $orderCount,
+        ]);
+
+        $customFields = $customer->getCustomFields() ?? [];
+        $customFields['custom_ordercount_copy'] = $orderCount;
+
+        $this->customerRepository->update([[
+            'id' => $customer->getId(),
+            'customFields' => $customFields,
+        ]], $context);
+    }
+}
